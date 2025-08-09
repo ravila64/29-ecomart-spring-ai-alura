a0fcab7050cba08195c80e5a34a4d5d0161d98bb
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-08-08 19:26:03 -0500

    add to app.properties+controller generar productos

---
+++ src/main/java/com/aluracursos/ecomart/controller/GeneradorDeProductosController.java
@@ -0,0 +1,27 @@
+package com.aluracursos.ecomart.controller;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/generador")
+public class GeneradorDeProductosController {
+
+   private final ChatClient chatClient;
+
+   public GeneradorDeProductosController(ChatClient.Builder chatClientBuilder) {
+      this.chatClient = chatClientBuilder.build();
+   }
+
+   @GetMapping   // estaba ("/ai")
+   public String generadorDeProductos(String userInput) {
+      var pregunta = "genera 5 productos ecológicos";
+      return this.chatClient.prompt()
+            .user(pregunta)  // userInput
+            .call() // llamada
+            .content(); // contenido de la respuesta
+   }
+}
