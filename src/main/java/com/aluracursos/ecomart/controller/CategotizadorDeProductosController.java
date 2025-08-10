dce15d40369d0e97e1444c4c50d5b25a3fcb749f
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-08-10 16:19:38 -0500

    continuar categorizacion

--- src/main/java/com/aluracursos/ecomart/controller/CategotizadorDeProductosController.java
+++ src/main/java/com/aluracursos/ecomart/controller/CategotizadorDeProductosController.java
@@ -17,12 +17,22 @@ public class CategotizadorDeProductosController {
 
    @GetMapping
    public String categorizarProductos(String producto) {
-      var system = "Tu eres un categorizador de productos";
+      var system = """
+            Tu eres un categorizador de productos y solo responder la categoría del producto informado,
+            selecciona una categoría de la lista:
+               1. Higiene personal
+               2. Electrónicos
+               3. Deportes
+               4. Otros
+               Ejemplo de uso:
+               Producto: Pelota de futbol
+               Respuesta: Deportes
+            """;
       return this.chatClient.prompt()
             .system(system)
             .user(producto)
             .options(ChatOptions.builder()
-                  .temperature(0.82)
+                  .temperature(0.82)  // 0.1 y 2.0
                   .build())
             .call()
             .content();
