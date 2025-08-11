e2e7615b236d6e7aa37f095300e94ce00d917b53
Rene Avila <rene.avila.alonso@gmail.com>
2025-08-10 20:08:36 -0500

    add dependencia externa+app properties+conteo de tokens

--- src/main/java/com/aluracursos/ecomart/controller/CategotizadorDeProductosController.java
+++ src/main/java/com/aluracursos/ecomart/controller/CategotizadorDeProductosController.java
@@ -1,5 +1,7 @@
 package com.aluracursos.ecomart.controller;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,12 @@ public class CategotizadorDeProductosController {
 
    private final ChatClient chatClient;
    public CategotizadorDeProductosController(ChatClient.Builder chatClientBuilder) {
-      this.chatClient = chatClientBuilder.build();
+      this.chatClient = chatClientBuilder
+              .defaultOptions(ChatOptions
+                     .builder()
+                      .model("gpt-4o-mini")
+                      .build()
+              ).build();
    }
 
    @GetMapping
@@ -28,13 +35,24 @@ public class CategotizadorDeProductosController {
                Producto: Pelota de futbol
                Respuesta: Deportes
             """;
+         var tokens = contadorDeTokens(system, producto);
+         System.out.println("Cantidad de tokens "+tokens);
+         // implementa la logica para la seleccion del modelo
       return this.chatClient.prompt()
             .system(system)
             .user(producto)
             .options(ChatOptions.builder()
+                  .model("gpt-4o-mini")  //usa primero este modelo
                   .temperature(0.82)  // 0.1 y 2.0
                   .build())
             .call()
             .content();
+   }
+
+   // este metodo tratar de colocarlo en una clase aparte, y se inyecta en los controllers necesarios
+   private int contadorDeTokens(String system, String user){
+      var registry = Encodings.newDefaultEncodingRegistry();  // type EncodingRegistry
+      var enc = registry.getEncodingForModel(ModelType.GPT_4O_MINI);  // tyoe Encoding
+      return enc.countTokens(system+user);
    }
 }
